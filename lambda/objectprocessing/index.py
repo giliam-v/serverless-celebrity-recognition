8ee07076a6325a0c2c4a015bae1ac3143324b46f
giliamv <giliamv@amazon.com>
2022-07-09 22:43:14 +0200

    improve function structure

--- lambda/objectprocessing/index.py
+++ lambda/objectprocessing/index.py
@@ -11,36 +11,60 @@ def handler(event, context):
     logger.debug('Request event:\n%s', json.dumps(event, indent=2, default=str))
     
     try:
-        bucket = event['Records'][0]['s3']['bucket']['name']
-        key = event['Records'][0]['s3']['object']['key']
-        date = event['Records'][0]['eventTime']
+        table = os.environ.get('DDBTable')
 
-        logger.info(f'Initiating celebrity recognition for S3 file: {bucket}/{key}')
-
-        response = boto3.client('rekognition').recognize_celebrities(
-            Image={
-                'S3Object': {
-                    'Bucket': bucket,
-                    'Name': key,
-                }
-            }
-        )
-
-        logger.debug('Result:\n%s', json.dumps(response, indent=2, default=str))
-
-        celeb = response['CelebrityFaces'][0]['Name']
-        logger.info(f'Celebrity: {celeb}')
-
-        ddbtable = os.environ.get('DDBTable')
-        logger.info(f'Writing metadata to table: {ddbtable}')
+        for record in event['Records']:
+            bucket = record['s3']['bucket']['name']
+            filename = record['s3']['object']['key']
+            date = record['eventTime']
+            
+            celebrity = get_celebrity(bucket, filename)
         
-        dbresponse = boto3.client('dynamodb').put_item(TableName=ddbtable, Item={'filename':{'S':key},'date':{'S':date},'celebrity':{'S':celeb}})
-        logger.debug('Result:\n%s', json.dumps(dbresponse, indent=2, default=str))
-
-
+            put_dynamodb_item(table, filename, date, celebrity)
     
     except Exception as e:
         logger.error(str(e))
     
     finally:
         print('Bye!')
+
+
+def get_celebrity(bucket, filename):
+    logger.info(f'Initiating celebrity recognition for S3 image: {bucket}/{filename}')
+
+    response = boto3.client('rekognition').recognize_celebrities(
+        Image={
+            'S3Object': {
+                'Bucket': bucket,
+                'Name': filename,
+            }
+        }
+    )
+    
+    logger.debug('Result:\n%s', json.dumps(response, indent=2, default=str))
+    celebrities = response['CelebrityFaces']
+
+    if len(celebrities) == 0:
+        celebrity = 'unknown'
+        logger.info('No celebrities found. Setting name to "unknown"')
+    else:
+        # For simplicity of demo we only record the first celebrity found
+        celebrity = celebrities[0]['Name']
+        logger.info(f'Celebrity found: {celebrity}')
+
+    return celebrity
+
+
+def put_dynamodb_item(table, filename, date, celebrity):
+    logger.info(f'Writing metadata to table: {table}')
+
+    response = boto3.client('dynamodb').put_item(
+        TableName=table,
+        Item={
+            'filename':{'S':filename},
+            'date':{'S':date},
+            'celebrity':{'S':celebrity}
+        }
+    )
+
+    logger.debug('Result:\n%s', json.dumps(response, indent=2, default=str))
